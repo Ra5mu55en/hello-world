0b0742789d70cfaf936507ec87432d570bfe4ffd
Ra5mu55en <anton@bebopinteractive.com>
2019-12-17 16:32:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-import csv
+from flask import Flask
+app = Flask(__name__)
 
-def main():
-  with open('innovators.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["SN", "Name", "Contribution"])
-    writer.writerow([1, "Linus Torvalds", "Linux Kernel"])
-    writer.writerow([2, "Tim Berners-Lee", "World Wide Web"])
-    writer.writerow([3, "Guido van Rossum", "Python Programming"])
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    app.run()
