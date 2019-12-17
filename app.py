8969b2b1906286dc17e1c623f92ccf9a79ce94a3
Ra5mu55en <anton@bebopinteractive.com>
2019-12-17 16:43:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, Response, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+@app.route("/submit/", methods=['GET', 'POST'])
+def move_forward():
+    #Moving forward code
+    forward_message = "Moving Forward..."
+    return render_template('index.html', forward_message=forward_message);
+
 
 if __name__ == "__main__":
     app.run()
