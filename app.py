6da615061548df4272963168ea27a9a9d2cfcebb
Ra5mu55en <anton@bebopinteractive.com>
2019-12-16 22:45:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
-from flask import Flask, render_template, redirect, url_for,request
-from flask import make_response
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "hi"
-@app.route("/index")
-def index():
-    return "yo"
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-   message = None
-   if request.method == 'POST':
-        datafromjs = request.form['mydata']
-        result = "return this"
-        resp = make_response('{"response": '+result+'}')
-        resp.headers['Content-Type'] = "application/json"
-        return resp
-        return render_template('login.html', message='')
-
-if __name__ == "__main__":
-    app.run(port=8080, debug = True)
+def hello():
+    return "Hello, World!"
